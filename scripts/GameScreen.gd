89a9085387af0d8f89ecb9e4b8629339029b810a
mikahimself <mika.laihanen@gmail.com>
2020-01-08 23:24:46 +0200

    Make snake to grow with food and die when hitting its own tail.

--- scripts/GameScreen.gd
+++ scripts/GameScreen.gd
@@ -2,5 +2,54 @@ extends Node2D
 
 var screen_w: int = 768
 var screen_h: int = 432
+var tile_size: int = 16
+var start_x: int = 0
+var start_y: int = 0
+var game_on: bool = true
+var move_time_step: float = 0.15
+var current_move_time: float = 0.0
 
-var tile_size: int = 8
+onready var snake_scene: PackedScene = preload("res://scenes/Snake.tscn")
+onready var fruit_scene: PackedScene = preload("res://scenes/Fruit.tscn")
+var snake: Node2D
+var fruit: Sprite
+
+func _ready():
+    spawn_snake()
+    spawn_fruit()
+
+func spawn_snake() -> void:
+    snake = snake_scene.instance()
+    snake.start_position = Vector2(start_x, start_y)
+    snake.connect("snake_died", self, "on_snake_dead")
+    snake.connect("ate_fruit", self, "on_snake_eat")
+    add_child(snake)
+
+func spawn_fruit() -> void:
+    fruit = fruit_scene.instance()
+    add_child(fruit)
+    move_fruit()
+    
+func move_fruit() -> void:
+    var cols = screen_w / tile_size
+    var rows = screen_h / tile_size
+    var new_col = randi() % cols
+    var new_row = randi() % rows
+    var new_pos = Vector2(new_col * tile_size, new_row * tile_size)
+    fruit.position = new_pos
+
+func _process(delta) -> void:
+    if game_on:
+        snake.check_input()
+        current_move_time += delta
+
+        if current_move_time >= move_time_step:
+            snake.move_snake()
+            snake.check_for_fruit(fruit.position)
+            current_move_time = 0
+
+func on_snake_dead():
+    game_on = false
+
+func on_snake_eat():
+    move_fruit()
--- scripts/Snake.gd
+++ scripts/Snake.gd
@@ -2,45 +2,46 @@ extends Node2D
 
 enum directions { UP, DOWN, LEFT, RIGHT }
 var direction
-var move_time_step = 0.025
-var current_move_time: float = 0.0
+var previous_direction
+
 var tile_size: int = 16
 var tiles: Array = []
 var tile_pos: Array = []
 var last_pos: Vector2 = Vector2.ZERO
+var start_position: Vector2
 
-onready var snake_tile = preload("res://scenes/SnakeTile.tscn")
+onready var snake_tile: PackedScene = preload("res://scenes/SnakeTile.tscn")
+onready var snake_tiles = $SnakeTiles
 
+signal snake_died()
+signal ate_fruit()
 
-func _ready():
-	var tile = snake_tile.instance()
-	tile.position = Vector2(position.x, position.y)
-	add_child(tile)
-	tiles.append(tile)
-	tile_pos.append(tile.position)
-	
+var alive: bool = false
+
+func _ready() -> void:
+	var head = snake_tile.instance()
+	head.position = start_position
+	snake_tiles.add_child(head)
+	tiles.append(head)
+	tile_pos.append(head.position)
 	direction = directions.RIGHT
 
-func _process(delta):
-	current_move_time += delta
+func check_input() -> void:
 
-	if current_move_time >= move_time_step:
-		move_snake()
-		current_move_time = 0
+	previous_direction = direction
 
-func _input(event):
-	if event.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") and direction != directions.RIGHT:
 		direction = directions.LEFT
-	if event.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") and direction != directions.LEFT:
 		direction = directions.RIGHT
-	if event.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") and direction != directions.DOWN:
 		direction = directions.UP
-	if event.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down") and direction != directions.UP:
 		direction = directions.DOWN
-	if event.is_action_pressed("ui_select"):
+	if Input.is_action_pressed("ui_select"):
 		eat_fruit()
 
-func move_snake():
+func move_snake() -> void:
 
 	last_pos = tile_pos[-1]
 	var new_pos = Vector2.ZERO
@@ -55,24 +56,37 @@ func move_snake():
 		directions.DOWN:
 			new_pos = Vector2(tiles[0].position.x, tiles[0].position.y + tile_size)
 	
-	if new_pos.x >= 772:
-		new_pos.x = 4
-	if new_pos.x <= -4:
-		new_pos.x = 772
-	if new_pos.y > 432:
-		new_pos.y = 4
+	if new_pos.x >= 768:
+		new_pos.x = 0
+	if new_pos.x < 0:
+		new_pos.x = 752
+	if new_pos.y > 416:
+		new_pos.y = 0
 	if new_pos.y < 0:
-		new_pos.y = 436
+		new_pos.y = 416
 
 	tile_pos.pop_back()
+	check_for_tail(new_pos)
 	tile_pos.push_front(new_pos)
 
 	for i in range(0, tiles.size()):
 		tiles[i].position = tile_pos[i]
+
+	print(tile_pos[0])
 	
+func check_for_fruit(fruit_position) -> void:
+	if fruit_position == tile_pos[0]:
+		eat_fruit()
+		emit_signal("ate_fruit")
+
+func check_for_tail(new_pos) -> void:
+	if tile_pos.has(new_pos):
+		alive = false
+		emit_signal("snake_died")
+
 func eat_fruit():
 	var tile = snake_tile.instance()
 	tile.position = last_pos
-	add_child(tile)
+	snake_tiles.add_child(tile)
 	tile_pos.append(tile.position)
 	tiles.append(tile)
