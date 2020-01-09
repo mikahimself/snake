6f94590879ffdb2b4016641847a97b46c6d51ae9
mikahimself <mika.laihanen@gmail.com>
2020-01-10 00:26:26 +0200

    Add snake images, move functionality to gamescreen, prevent fruit from spawning under snake.

--- scripts/GameScreen.gd
+++ scripts/GameScreen.gd
@@ -3,10 +3,11 @@ extends Node2D
 var screen_w: int = 768
 var screen_h: int = 432
 var tile_size: int = 16
-var start_x: int = 0
-var start_y: int = 0
+var start_x: int = 24
+var start_y: int = 24
+var offset: int = 8
 var game_on: bool = true
-var move_time_step: float = 0.15
+var move_time_step: float = 0.1
 var current_move_time: float = 0.0
 
 onready var snake_scene: PackedScene = preload("res://scenes/Snake.tscn")
@@ -15,6 +16,7 @@ var snake: Node2D
 var fruit: Sprite
 
 func _ready():
+    randomize()
     spawn_snake()
     spawn_fruit()
 
@@ -35,8 +37,12 @@ func move_fruit() -> void:
     var rows = screen_h / tile_size
     var new_col = randi() % cols
     var new_row = randi() % rows
-    var new_pos = Vector2(new_col * tile_size, new_row * tile_size)
-    fruit.position = new_pos
+    var new_pos = Vector2(new_col * tile_size + offset, new_row * tile_size + offset)
+
+    if snake.is_fruit_under_snake(new_pos):
+        move_fruit()
+    else:
+        fruit.position = new_pos
 
 func _process(delta) -> void:
     if game_on:
@@ -44,7 +50,8 @@ func _process(delta) -> void:
         current_move_time += delta
 
         if current_move_time >= move_time_step:
-            snake.move_snake()
+            snake.calculate_snake_position()
+            snake.draw_snake()
             snake.check_for_fruit(fruit.position)
             current_move_time = 0
 
--- scripts/Snake.gd
+++ scripts/Snake.gd
@@ -11,6 +11,12 @@ var last_pos: Vector2 = Vector2.ZERO
 var start_position: Vector2
 
 onready var snake_tile: PackedScene = preload("res://scenes/SnakeTile.tscn")
+onready var snake_corner_12 = preload("res://art/snake-corner-12.png")
+onready var snake_corner_18 = preload("res://art/snake-corner-18.png")
+onready var snake_corner_84 = preload("res://art/snake-corner-84.png")
+onready var snake_corner_42 = preload("res://art/snake-corner-42.png")
+onready var snake_body = preload("res://art/snake-tile.png")
+onready var snake_head = preload("res://art/snake-head-tile.png")
 onready var snake_tiles = $SnakeTiles
 
 signal snake_died()
@@ -21,6 +27,7 @@ var alive: bool = false
 func _ready() -> void:
 	var head = snake_tile.instance()
 	head.position = start_position
+	head.set_texture(snake_head)
 	snake_tiles.add_child(head)
 	tiles.append(head)
 	tile_pos.append(head.position)
@@ -41,9 +48,7 @@ func check_input() -> void:
 	if Input.is_action_pressed("ui_select"):
 		eat_fruit()
 
-func move_snake() -> void:
-
-	last_pos = tile_pos[-1]
+func calculate_snake_position() -> void:
 	var new_pos = Vector2.ZERO
 
 	match direction:
@@ -55,29 +60,95 @@ func move_snake() -> void:
 			new_pos = Vector2(tiles[0].position.x, tiles[0].position.y - tile_size)
 		directions.DOWN:
 			new_pos = Vector2(tiles[0].position.x, tiles[0].position.y + tile_size)
-	
-	if new_pos.x >= 768:
-		new_pos.x = 0
+
+	# Pass to other side of screen
+	if new_pos.x >= 776:
+		new_pos.x = 8
 	if new_pos.x < 0:
-		new_pos.x = 752
-	if new_pos.y > 416:
-		new_pos.y = 0
+		new_pos.x = 760
+	if new_pos.y > 424:
+		new_pos.y = 8
 	if new_pos.y < 0:
-		new_pos.y = 416
+		new_pos.y = 424
 
 	tile_pos.pop_back()
 	check_for_tail(new_pos)
 	tile_pos.push_front(new_pos)
 
-	for i in range(0, tiles.size()):
+func draw_snake() -> void:
+	for i in range(1, tiles.size()):
 		tiles[i].position = tile_pos[i]
+		if i > 0:
+			tiles[i].set_texture(snake_body)
+			tiles[0].flip_h = false
+			if tile_pos[i - 1].x < tile_pos[i].x:
+				tiles[i].rotation_degrees = 0
+				var next_tile = i + 1
+				if next_tile < tile_pos.size():
+					if tile_pos[i + 1].y < tile_pos[i].y:
+						tiles[i].set_texture(snake_corner_18)
+					elif tile_pos[i + 1].y > tile_pos[i].y:
+						tiles[i].set_texture(snake_corner_84)
+			elif tile_pos[i - 1].x > tile_pos[i].x:
+				tiles[i].rotation_degrees = 0
+				var next_tile = i + 1
+				if next_tile < tile_pos.size():
+					if tile_pos[i + 1].y < tile_pos[i].y:
+						tiles[i].set_texture(snake_corner_12)
+					elif tile_pos[i + 1].y > tile_pos[i].y:
+						tiles[i].set_texture(snake_corner_42)
 
-	print(tile_pos[0])
+			elif tile_pos[i - 1].y < tile_pos[i].y:
+				var next_tile = i + 1
+				if next_tile < tile_pos.size():
+					if tile_pos[i + 1].x < tile_pos[i].x:
+						tiles[i].set_texture(snake_corner_18)
+					elif tile_pos[i + 1].x > tile_pos[i].x:
+						tiles[i].set_texture(snake_corner_12)
+					else:
+						tiles[i].rotation_degrees = 90
+				else:
+					tiles[i].rotation_degrees = 90
+			elif tile_pos[i - 1].y > tile_pos[i].y:
+				var next_tile = i + 1
+				if next_tile < tile_pos.size():
+					if tile_pos[i + 1].x > tile_pos[i].x:
+						tiles[i].set_texture(snake_corner_42)
+					elif tile_pos[i + 1].x < tile_pos[i].x:
+						tiles[i].set_texture(snake_corner_84)
+					else:
+						tiles[i].rotation_degrees = 90
+				else:
+					tiles[i].rotation_degrees = 90
+						
+	set_snake_head()
+
+func set_snake_head() -> void:
+	tiles[0].position = tile_pos[0]
+	match direction:
+		directions.LEFT:
+			tiles[0].rotation_degrees = 0
+			tiles[0].flip_h = true
+		directions.RIGHT:
+			tiles[0].rotation_degrees = 0
+			tiles[0].flip_h = false
+		directions.UP:
+			tiles[0].rotation_degrees = -90
+			tiles[0].flip_h = false
+		directions.DOWN:
+			tiles[0].rotation_degrees = 90
+			tiles[0].flip_h = false
 	
 func check_for_fruit(fruit_position) -> void:
 	if fruit_position == tile_pos[0]:
 		eat_fruit()
 		emit_signal("ate_fruit")
+
+func is_fruit_under_snake(fruit_position) -> bool:
+	if tile_pos.has(fruit_position):
+		return true
+	else:
+		return false
 
 func check_for_tail(new_pos) -> void:
 	if tile_pos.has(new_pos):
